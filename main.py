e9a5e0b63f97af23681729205a345977455b0d79
jayzuus <jayzoom122@gmail.com>
2022-05-06 23:18:39 -0700

    uniquetokens

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import time
+import argparse
 
-print("Hello World")
+
+
+
+
+def main():
+    occurences = dict()
+
+    occurences["<STOP>"] = 0
+    occurences["<UNK>"] = 0
+    f = open("/Users/jaypatel/Desktop/A2-Data/Assignment2/A2-Data/1b_benchmark.train.tokens", "r")
+    for line in f:  
+        for word in line.split():
+            if word in occurences:
+                occurences[word] += 1
+            else:
+                occurences[word] = 1
+            
+    for key in list(occurences.keys()):
+        if occurences[key] < 3 and key != '<UNK>' and key != '<STOP>':
+            occurences.pop(key)
+            occurences["<UNK>"]+= 1
+       
+    print(len(occurences))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
